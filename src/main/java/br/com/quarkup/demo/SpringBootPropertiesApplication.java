d31539700179bdf3321bda2c42bc74ce1e97f0ab
Fabio Biasi Mello Rubim <fabiorubim@hotmail.com>
2020-12-07 23:10:48 -0300

    Exemplo do livro SpringBoot - Acelere o desenvolvimento de microsserviços

---
+++ src/main/java/br/com/quarkup/demo/SpringBootPropertiesApplication.java
@@ -0,0 +1,21 @@
+package br.com.quarkup.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringBootPropertiesApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPropertiesApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootPropertiesApplication.class);
+    }
+
+}
---
+++ src/test/java/br/com/quarkup/demo/SpringBootPropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+//package br.com.quarkup.demo;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SpringBootPropertiesApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
